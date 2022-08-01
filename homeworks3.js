02fb49ce283ef06d5eb78275fa966368bf984b75
hungvnhd <104383399+hungvnhd@users.noreply.github.com>
2022-08-02 02:59:17 +0700

    Add files via upload

---
+++ homeworks3.js
@@ -0,0 +1,111 @@
+//bài tập 1 :
+// for(i=1;i<=100;i=i+1){
+//     console.log(i);
+// }   if (i===99){
+   
+// }   console.log("Đã hoàn thành");
+
+
+
+
+// bài tập 2:
+
+
+// let n1=0, n2 = 1, n3;
+// let fi=""
+//         for (let i=1; i<=20; i=i+1){
+//             n3=n1+n2;
+//             n1=n2;
+//             n2=n3
+//             fi=fi+ n3 + " "
+            
+// }   console.log(`0 1 `+fi);
+
+
+
+// bài tập 3:
+// let n1 = 0 , n2= 0, n3=0
+//         let a 
+//         for (let i=0; i<10;i=i+1){
+//             if(i===0){
+//                 n1=1
+//                 n3=1
+//         }    else{
+
+//                 n1= n2+n3
+//                 n2 = n3
+//                 n3 = n1
+               
+                
+//                 if(n1%5===0){
+//                     a = n1
+//                     break
+//                } 
+//          }   
+// }   console.log("Sô chia hết cho 5 đầu tiên trong dãy là "+a);
+
+
+       
+
+
+// bài tập 4:
+// let n1 = 0 , n2= 0, n3=0, a=0
+// for (let i=0; i<=20;i=i+1){
+//     if(i==0){
+//         n1=1
+//         n3=1
+//         a =a+n1
+//     }else{
+//         n1= n2+n3
+//         n2 = n3
+//         n3 = n1
+//         a= a+ n1
+        
+//     }
+// }
+// console.log("Tổng 20 số đầu tiên trong dãy fibonacci là = "+a);
+//bài tập 5
+// let a = 0;
+// for(i=7;i<=210;i=i+7){
+//     a=a+i
+    
+// }   console.log(`Tổng 30 số chia hết cho 7 đầu tiên trong dãy số tự nhiên là `+ a);
+
+// // bài tập 6
+
+// for(let i=1;i<=100;i=i+1){
+//     if(i%3===0 && i%5===0){
+//     console.log(`FizzFuzz`);
+// }   else if(i%3===0){
+//     console.log(`Fizz`);
+// }   else if(i%5===0){
+//     console.log(`Fuzz`);
+// }
+
+// }
+
+//bài tập 7:
+// let a=1
+// let n=Number(prompt("mời nhập số nguyên dương n"));
+
+// if(n>0 && n%1===0){
+//     for(i=1;i<=n;i=i+1){
+//         a=a*i;
+
+//     } console.log(`Giai thừa của số nguyên dương ` +n+  " là " + a);
+
+
+// }
+
+
+
+
+
+    
+    
+
+
+
+
+
+
